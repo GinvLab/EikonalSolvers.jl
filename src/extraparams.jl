b75672a88985ea3291a8dbda3b05387fad3a5f6c
Andrea Zunino <inverseproblem@users.noreply.github.com>
2020-09-17 21:09:57 +0200

    add function to set ExtraParams

---
+++ src/extraparams.jl
@@ -0,0 +1,33 @@
+
+
+##--------------------------------------------------------------
+## allowfixsqarg: if true, try to brute-force fix problems with
+##                negative sqarg.
+##    Use is discouraged...
+Base.@kwdef mutable struct ExtraParams
+    ## brute-force fix negative sqarg
+    allowfixsqarg::Bool
+    ## refine grid around source?
+    refinearoundsrc::Bool
+end
+
+##--------------------------
+function setextraparams!(extrapars::ExtraParams ;
+                         allowfixsqarg::Bool, refinearoundsrc::Bool)
+    extrapars.allowfixsqarg = allowfixsqarg
+    extrapars.refinearoundsrc = refinearoundsrc
+    warningextrapar(extrapars)
+    return 
+end
+##--------------------------
+function warningextrapar(extrapars::ExtraParams)
+    if extrapars.allowfixsqarg==true
+        @warn("ExtraParams: allowfixsqarg==true, brute-force fixing of negative discriminant allowed.")
+    end
+    if extrapars.refinearoundsrc==false
+        @warn("ExtraParams: refinearoundsrc==false, no grid refinement around source.")
+    end
+    return 
+end
+
+##--------------------------------------------------------------
