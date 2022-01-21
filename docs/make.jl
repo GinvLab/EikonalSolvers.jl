d1e8af7698eb8784597f83cb4ed694c9ae767040
Andrea Zunino <6928707-anzun@users.noreply.gitlab.com>
2022-01-21 11:29:52 +0100

    test deployment of docs

--- docs/make.jl
+++ docs/make.jl
@@ -15,5 +15,28 @@ makedocs(modules = [EikonalSolvers],
          )
 
 deploydocs(
-    repo="gitlab.com/JuliaGeoph/EikonalSolvers.jl.git",
+    repo="gitlab.com/JuliaGeoph/EikonalSolvers.jl/blob/{commit}{path}#{line}",
+    #repo="../../EikonalSolvers.jl",
+    deploy_config = Documenter.GitLab(),
 )
+
+
+# GitLab <: DeployConfig
+
+# GitLab implementation of DeployConfig.
+
+# The following environment variables influence the build when using the GitLab configuration:
+
+#     DOCUMENTER_KEY: must contain the Base64-encoded SSH private key for the repository. This variable should be set in the GitLab settings. Make sure this variable is marked NOT to be displayed in the build log.
+
+#     CI_COMMIT_BRANCH: the name of the commit branch.
+
+#     CI_EXTERNAL_PULL_REQUEST_IID: Pull Request ID from GitHub if the pipelines are for external pull requests.
+
+#     CI_PROJECT_PATH_SLUG: The namespace with project name. All letters lowercased and non-alphanumeric characters replaced with -.
+
+#     CI_COMMIT_TAG: The commit tag name. Present only when building tags.
+
+#     CI_PIPELINE_SOURCE: Indicates how the pipeline was triggered.
+
+# The CI_* variables are set automatically on GitLab. More information on how GitLab sets the CI_* variables can be found in the GitLab documentation.
